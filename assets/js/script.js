fb8634770fb39601e7ef017eae3b83352ff6177e
adamlarue19 <111079858+adamlarue19@users.noreply.github.com>
2022-10-15 23:15:20 -0400

    made changes to the search bar and button along with some js additions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,7 @@
 // global variables
-// DOM references
-var searchInputEl = document.querySelector('.input')
-var imgEl = document.querySelector('.game-img');
-var titleEl = document.querySelector('.game-title');
-var genreEl = document.querySelector('.game-genre');
-var releaseEl = document.querySelector('.game-release');
-var platformEl = document.querySelector('.game-platform');
+var apiUrl = 'https://rawg-video-games-database.p.rapidapi.com/games' + apiKey
 
+var apiKey = '?key=9c9c4ff2f104433ba2fee0058fd0a4bd'
 // RAWG API
 const options = {
 	method: 'GET',
@@ -16,30 +11,37 @@ const options = {
 	}
 };
 
-fetch('https://rawg-video-games-database.p.rapidapi.com/games?key=9c9c4ff2f104433ba2fee0058fd0a4bd',options)
+  fetch('https://rawg-video-games-database.p.rapidapi.com/games?key=9c9c4ff2f104433ba2fee0058fd0a4bd',options)
 	.then(response => response.json())
-  .then((completedata)=>{
-    let data1='';
-    completedata.map((values)=>{
-      data1=``
-    })
-
-  })
+  .then(response => console.log(response))
 	.catch(err => console.error(err));
 
+//   // Youtube API
+// const youtube = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': '4185a86540msh55a8448ac6e5678p149c46jsna834019ba73d',
+//     'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+//   }
+// };
   
-// Youtube API
-const youtube = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '4185a86540msh55a8448ac6e5678p149c46jsna834019ba73d',
-    'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-  }
-};
-  
-fetch('https://youtube138.p.rapidapi.com/channel/videos/?id=UCJ5v_MCY6GNUBTO8-D3XoAg&hl=en&gl=US', youtube)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+// fetch('https://youtube138.p.rapidapi.com/channel/videos/?id=UCJ5v_MCY6GNUBTO8-D3XoAg&hl=en&gl=US', youtube)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
 
+
+// DOM references
+var gameInfoEl = document.getElementById('game-info');
+var searchInputEl = document.querySelector('.input');
+var imgEl = document.querySelector('.game-img');
+
+// function to display the searched game information
+function renderSearchResults(gameName, response) {
+
+gameInfoEl.innerHTML = '';
+gameInfoEl.setAttribute('class', 'container')
+
+
+gameName = gameName.charAt(0).toUpperCase() + gameName.slice(1);
 
