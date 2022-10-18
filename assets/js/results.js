6ca968c4e292d13e1ddc0e4de74e1e80e7ae4274
adamlarue19 <111079858+adamlarue19@users.noreply.github.com>
2022-10-18 13:41:28 -0400

    made local storage and other stuff

---
+++ assets/js/results.js
@@ -0,0 +1,23 @@
+var imgEl = document.getElementById('game-img');
+var descriptionEl = document.getElementById('game-description')
+var genreEL;
+
+function renderItems() {
+    var img = localStorage.getItem('game-img');
+    var description = localStorage.getItem('game-description');
+    //platform
+    //genre
+
+
+    imgEl.setAttribute("src", img)
+    descriptionEl.textContent = description;
+    var platforms = localStorage.getItem("platforms").split(",")
+    for (let index = 0; index < platforms.length; index++) {
+        const element = platforms[index];
+        const p = document.createElement("p")
+        p.textContent = element
+        descriptionEl.appendChild(p)
+
+    }
+}
+renderItems();
