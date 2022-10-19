197c993a4c210deed386fd451808443afa5f7954
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-10-18 23:14:31 -0400

    debugged data rendering

--- assets/js/results.js
+++ assets/js/results.js
@@ -6,21 +6,21 @@ var descriptionEl = document.getElementById('game-description')
 function renderItems() {
     var img = localStorage.getItem('game-img');
     var description = localStorage.getItem('game-description');
-    var platforms = localStorage.getItem("platforms").split(",");
-    var genres = localStorage.getItem("genres").split(",");
+    var platforms = localStorage.getItem("platforms").split('[]');
+    var genres = localStorage.getItem("genres").split("[]");
 
     imgEl.setAttribute("src", img)
-    descriptionEl.textContent = description;
+    descriptionEl.textContent = "Description: " + description;
     for (let index = 0; index < platforms.length; index++) {
         const element = platforms[index];
         const p = document.createElement("p")
-        p.textContent = element
+        p.textContent = "Platforms: " + element;
         descriptionEl.appendChild(p)
     }
     for (let index = 0; index < genres.length; index++) {
         const element = genres[index];
         const p = document.createElement("p")
-        p.textContent = element
+        p.textContent = "Genres: " + element;
         descriptionEl.appendChild(p)
     }
 }
