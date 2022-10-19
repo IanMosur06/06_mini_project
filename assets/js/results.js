945e4e0cf02e0d7ffb2749a090f749dc3b46b6c4
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-10-18 20:41:37 -0400

    formatted js structure, debugged renderitems, added genre render

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,24 +1,30 @@
+// DOM references
 var imgEl = document.getElementById('game-img');
 var descriptionEl = document.getElementById('game-description')
-var genreEL;
 
+// function to render data onto the page
 function renderItems() {
     var img = localStorage.getItem('game-img');
     var description = localStorage.getItem('game-description');
-    //platform
-    //genre
-
+    var platforms = localStorage.getItem("platforms").split(",");
+    var genres = localStorage.getItem("genres").split(",");
 
     imgEl.setAttribute("src", img)
     descriptionEl.textContent = description;
-    var platforms = localStorage.getItem("platforms").split(",")
     for (let index = 0; index < platforms.length; index++) {
         const element = platforms[index];
         const p = document.createElement("p")
         p.textContent = element
         descriptionEl.appendChild(p)
-
+    }
+    for (let index = 0; index < genres.length; index++) {
+        const element = genres[index];
+        const p = document.createElement("p")
+        p.textContent = element
+        descriptionEl.appendChild(p)
     }
 }
+
+// calls function above and fetch youtube api from script.js
 renderItems();
 getYoutubeId();
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,34 +4,11 @@ var storage = {
   genreArray: [],
   rating: [],
 };
-// rawg api key
-var apiKey = '?key=9c9c4ff2f104433ba2fee0058fd0a4bd';
 
-// RAWG API
-const options = {
-  method: 'GET',
-  // headers: {
-  //   'X-RapidAPI-Key': '4185a86540msh55a8448ac6e5678p149c46jsna834019ba73d',
-  //   'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com'
-  // }
-};
-
-// Youtube API
-const youtube = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '39cd710cfbmsh9cc223551facc72p1446a3jsn33a268761098',
-    'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-  }
-};
-
-
-
-
-
-// DOM references
+// DOM reference
 var searchBtn = document.querySelector('.custom-button')
 
+// function to store data from api inside local storage
 function appendToLocalStorage(data1, data2) {
   console.log("data1", data1)
   console.log("data2", data2)
@@ -50,9 +27,33 @@ function appendToLocalStorage(data1, data2) {
   localStorage.setItem('game-description', description);
   localStorage.setItem('platforms', JSON.stringify(storage.platformArray));
   localStorage.setItem('genres', JSON.stringify(storage.genreArray));
+  window.location.href="results.html";
 }
 
+// RAWG API
+const options = {
+  method: 'GET',
+};
 
+// function to fetch game title and details
+async function fetchGameTitle(search) {
+console.log(search)
+  await fetch(`https://api.rawg.io/api/games/${search}?key=9c9c4ff2f104433ba2fee0058fd0a4bd`, options)
+    .then(response => response.json())
+    .then(response => {
+      if (response.redirect) {
+        console.log("redirecting...");
+        var slug = response.slug;
+        getGameFromSlug(slug);
+      }else{
+        console.log("no redirect");
+        appendToLocalStorage(response.parent_platforms, response);
+      }
+    })
+  .catch(err => console.error(err));
+}
+
+// function to fetch game title and details using slug
 async function getGameFromSlug(slug) { 
   console.log(slug)
   await fetch(`https://api.rawg.io/api/games/${slug}?key=9c9c4ff2f104433ba2fee0058fd0a4bd`, options)
@@ -65,7 +66,16 @@ async function getGameFromSlug(slug) {
     .catch(err => console.error(err));
 }
 
+// Youtube API
+const youtube = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '39cd710cfbmsh9cc223551facc72p1446a3jsn33a268761098',
+    'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+  }
+};
 
+// function to fetch youtube video link
 async function getYoutubeId () {
  var getGameName = localStorage.getItem("gameSearch");
  console.log(getGameName);
@@ -76,33 +86,11 @@ async function getYoutubeId () {
        console.log(getID);
        var videoPlayer = document.getElementById('player');
        videoPlayer.setAttribute("src",`https://www.youtube.com/embed/${getID}`);
-       
-       
   })
 	.catch(err => console.error(err))
 }
 
-
-// fetch game title and details
-async function fetchGameTitle(search) {
-console.log(search)
-  await fetch(`https://api.rawg.io/api/games/${search}?key=9c9c4ff2f104433ba2fee0058fd0a4bd`, options)
-    .then(response => response.json())
-    .then(response => {
-      if (response.redirect) {
-        console.log("redirecting...")
-        var slug = response.slug
-        getGameFromSlug(slug)
-
-      }else{
-        console.log("no redirect")
-
-        appendToLocalStorage(response.parent_platforms, response);
-      }
-    })
-    .catch(err => console.error(err));
-}
-
+//function to handle form submit
 function handeSearchFormSubmit(event) {
   event.preventDefault();
   var gameSearch = document.getElementById('search').value;
@@ -112,31 +100,9 @@ function handeSearchFormSubmit(event) {
   } else {
     fetchGameTitle(gameSearch);
     getYoutubeId(gameSearch);
-   localStorage.setItem("gameSearch",gameSearch);
+    localStorage.setItem("gameSearch",gameSearch);
   }
-  window.location.href="results.html"
 }
 
-
+// event listener to listen for a "click" on search button
 searchBtn.addEventListener('click', handeSearchFormSubmit)
-
-// localStorage.setItem("game-data", JSON.stringify(localStorage));
-
-// if(localStorage.getItem("game-data") !== null){
-//   var searchedGamed = JSON.parse(localStorage.getItem("game-data"));
-// }else {
-//   console.log("error, Nothing in storage");
-// }
-
-// // user input function
-// function handleGameSearch() {
-
-
-//   // if input full call fetch coords
-//   var search = inputEl.value;
-//   fetchgame(search);
-//   inputEl.value = "";
-// }
-
-
-
